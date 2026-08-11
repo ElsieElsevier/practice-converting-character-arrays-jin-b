04cecb9d5d1d04139e68aacb6dee3f90d13265b0
Jin <142944786+ElsieElsevier@users.noreply.github.com>
2026-08-10 23:52:51 -0500

    Practice: Using Arrays - 4 - Converting Character Arrays

--- using-arrays-4.js
+++ using-arrays-4.js
@@ -1,1 +1,29 @@
 
+// Task 1: 
+
+let message1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+
+let decoded1 = message1.split("").reverse().join("");
+let decoded2 = message2.split("").reverse().join("");
+let decoded3 = message3.split("").reverse().join("");
+let decoded4 = message4.split("").reverse().join("");
+
+console.log(decoded1);
+console.log(decoded2);
+console.log(decoded3);
+console.log(decoded4);
+
+
+// Task 2:
+
+let inspiration1 = "Keep learning even when it feels difficult.";
+let inspiration2 = "Small progress is still progress.";
+
+let reversed1 = inspiration1.split("").reverse().join("");
+let reversed2 = inspiration2.split("").reverse().join("");
+
+console.log(reversed1);
+console.log(reversed2);
